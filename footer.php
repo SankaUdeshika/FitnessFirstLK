660eceb06bc30c1309c05c57d91e825fa1094a54
Sanka Udeshika <sankaudeshika123@gmail.com>
2023-12-21 01:12:57 +0530

    Done Desing Footer Page

---
+++ footer.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer | fitness</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-white footerCover">
+                <div class="row">
+
+                    <!-- left -->
+
+                    <div class="col-6">
+                        <div class="row">
+
+                            <div class="col-6 text-center">
+                                <div class="row">
+                                    <div class="col-12 mt-3 mb-2    ">
+                                        <span class="footerTOpic">SITE MAP</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="row">
+                                    <div class="col-12 mt-5">
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                        <span>Lorem ipsum</span><br>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-10 offset-2 mt-3 mb-5">
+                                <span class="SocialIcons"><i class="bi bi-facebook"></i></span>
+                                <span class="SocialIcons" style="margin-left: 10px;"><i class="bi bi-instagram"></i></span>
+                                <span class="SocialIcons" style="margin-left: 10px;"><i class="bi bi-tiktok"></i></span>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- right -->
+                    <div class="col-6">
+                        <div class="row">
+
+                            <div class="col-6">
+                                <div class="row">
+                                    <div class="col-12 mt-5">
+                                        <span>LOCATION 01</span><br>
+                                        <span>Address, lorem ipsum</span><br>
+                                        <span>doleer se amet</span><br>
+                                        <span>+94 11 2 123 123</span><br>
+                                        <span>+94 77 123 123</span><br>
+                                    </div>
+                                    <div class="col-12 mt-3">
+                                        <span>LOCATION 03</span><br>
+                                        <span>Address, lorem ipsum</span><br>
+                                        <span>doleer se amet</span><br>
+                                        <span>+94 11 2 123 123</span><br>
+                                        <span>+94 77 123 123</span><br>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="row">
+                                    <div class="col-12 mt-5">
+                                        <span>LOCATION 02</span><br>
+                                        <span>Address, lorem ipsum</span><br>
+                                        <span>doleer se amet</span><br>
+                                        <span>+94 11 2 123 123</span><br>
+                                        <span>+94 77 123 123</span><br>
+                                    </div>
+                                    <div class="col-12 mt-3">
+                                        <span>LOCATION 04</span><br>
+                                        <span>Address, lorem ipsum</span><br>
+                                        <span>doleer se amet</span><br>
+                                        <span>+94 11 2 123 123</span><br>
+                                        <span>+94 77 123 123</span><br>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <hr class="border border-1 border-white">
+                    </div>
+
+                    <div class="col-12 text-center text-white-50">
+                        <span>
+                            © 2023 FITNESS FIRST LK. All Right Reserved Developed By Sanka Udeshika</span>
+                    </div>
+
+                </div>
+            </div>
+            <br>
+
+        </div>
+    </div>
+</body>
+
+</html>
