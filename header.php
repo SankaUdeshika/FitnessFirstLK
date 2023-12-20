66191e21bfa44319df001952d65de3540c64dcfb
Sanka Udeshika <sankaudeshika123@gmail.com>
2023-12-20 18:25:33 +0530

    Add Js Files, Add Preloader File, include it on Index.php file, Done some Changes on Style.css file and programe Preloader process on script.js

--- header.php
+++ header.php
@@ -1,11 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Header -->
+            <div class="col-12 headerCover">
+                <div class="row">
+
+                    <div class="col-3 text-center LogoCover">
+                        <span class="FitnesFirstTextLogo1white">FITNESS </span><span class="FitnesFirstTextLogored">FIRST LK</span>
+                    </div>
+
+                    <div class="col-7 text-center SectionCover">
+                        <div class="row">
+                            <div class="col-3">
+                                <span>MEMBERSHIPS</span>
+                            </div>
+                            <div class="col-3">
+                                <span>CLASSES</span>
+                            </div>
+                            <div class="col-3">
+                                <span>FACULITIES</span>
+                            </div>
+                            <div class="col-3">
+                                <span>BLOG</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <!-- Flex Logo -->
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
+
 </html>
