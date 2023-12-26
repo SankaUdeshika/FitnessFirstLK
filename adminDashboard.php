956994ebd547627ab7c32b729b6eda666e1bdf81
Amashwinx <113287402+Amashwinx@users.noreply.github.com>
2023-12-26 20:31:48 +0530

    changed title in admin Dashboard.php

--- adminDashboard.php
+++ adminDashboard.php
@@ -9,7 +9,7 @@ if (isset($_SESSION["admin"])) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Contact | RASIKA OFFICIAL</title>
+        <title>Admin Dashboard | RASIKA OFFICIAL</title>
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
