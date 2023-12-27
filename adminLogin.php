9f5680297469a378ccfc0ea5bf339ef6576bed62
Amashwinx <113287402+Amashwinx@users.noreply.github.com>
2023-12-27 22:01:24 +0530

    change title on adminLogin.php file

--- adminLogin.php
+++ adminLogin.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login | RASIKA OFFICIAL</title>
+    <title>Admin Login testing | RASIKA OFFICIAL</title>
     <link rel="stylesheet" href="css/bootstrap.css"/>
     <link rel="stylesheet" href="css/style.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"/>
