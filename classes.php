8fde22117475429d4711e50c4ccaefb92f9a1eda
Sanka Udeshika <sankaudeshika123@gmail.com>
2023-12-27 14:12:28 +0530

    Done Add  classes.php file and Design the classess intro

---
+++ classes.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classess | fitness First lk</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body class="bg-black">
+
+    <!-- preloader -->
+    <div class="col-12 preloader " id="preloader">
+        <?php include "preloader.php" ?>
+    </div>
+
+    <!-- Header -->
+    <div class="col-12" style="position: fixed; z-index: 4;">
+        <?php include "header.php" ?>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Top bar -->
+            <div class="col-12 Classes-cover  ">
+                <div class="row">
+                    <div class="col-12 mx-5">
+                        <div class="row ">
+                            <div class="col-12">
+                                <span class="fs-4 fw-bold text-white" style="font-family: monospace;">CHECK OUT</span>
+                            </div>
+
+                            <div class="col-12">
+                                <span class="Blog-Search-text">Our Classes</span><br>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Classes intro -->
+            <div class="col-10 offset-lg-1 mb-5">
+                <div class="col-12 mt-5">
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="Resources/images/gym02.jpg" class="classesVideo" alt="">
+                        </div>
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-12">
+                                    <span class="fw-bold text-white fs-2">Experience classes at Fitness First</span>
+                                </div>
+                                <div class="col-12">
+                                    <p class="text-white-50 ">
+                                        Whether you want to get leaner, stronger, faster, or just have fun while working out, we have something for everyone with our group Fitness Classes. There are 55 classes for you to try out across five different categories, including Cardio, Strength, Cycle, Mind and Body, and Fight.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+
+    <?php include "footer.php" ?>
+    <script src="js/script.js">
+    </script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
