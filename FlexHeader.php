9e3803276933daab8548e91fda2b21456f5429eb
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-02-13 21:54:59 +0530

    Design Basic Header

--- FlexHeader.php
+++ FlexHeader.php
@@ -11,7 +11,25 @@
 </head>
 
 <body>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- hedderCover -->
+            <div class="col-12 bg-black text-white">
+                <div class="row">
+                    <div class="col-4 d-flex  align-items-center">
+                        <span class="fw-bold fs-4 offset-4"><i class="bi bi-search"></i></span>
+                    </div>
+                    <div class="col-4 text-center d-flex justify-content-center">
+                        <span class="fs-1 fw-bold">Flex</span>
+                    </div>
+                    <div class="col-4 text-center">
+                        <span class="fs-1 fw-bold offset-4"><i class="bi bi-cart4"></i></span>
+                    </div>
+                </div>
+            </div>
 
+        </div>
+    </div>
 </body>
 
 </html>
