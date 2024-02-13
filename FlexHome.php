8a78fe242e28bc3dbc315ec5c400711e08d2a94d
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-02-13 17:55:09 +0530

    Create Flex Home and add Preloader and Add TopRedBorder

---
+++ FlexHome.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flex Home | Fitness First LK</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+
+    <!-- preloader -->
+    <div class="col-12 preloader " id="preloader">
+        <?php include "preloader.php" ?>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <!-- Top red Bar -->
+            <div class="col-12" style="background-color:red;">
+                <div class="row ">
+                    <div class="col-4">
+                    </div>
+                    <div class="col-4 text-center mt-3 mb-3 ">
+                        <span class="FlexTopRedBarTopic">Exclusive Suppliment🔥</span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Flex Header -->
+<div class="col-12">
+    <div class="row">
+        <?php require "FlexHeader.php"?>
+    </div>
+</div>
+
+
+        </div>
+    </div>
+
+    <?php include "footer.php" ?>
+    <script src="js/script.js">
+    </script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
