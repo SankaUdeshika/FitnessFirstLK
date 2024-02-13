b9605830fe67066e44c50b59cffc43052d016b62
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-02-13 17:54:22 +0530

    Create Flex Header php file

---
+++ FlexHeader.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flex Home | Fitness First LK</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
