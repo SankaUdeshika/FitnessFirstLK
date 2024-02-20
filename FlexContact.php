e1210006b8a420fc081c81db65e7ca13a8dd8865
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-02-20 15:00:47 +0530

    Done Design Flex Conatact Page

---
+++ FlexContact.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flex Contact | Fitness First LK</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body class="bg-black">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <!-- preloader -->
+                    <div class="col-12 preloader " id="preloader">
+                        <?php include "preloader.php" ?>
+                    </div>
+
+                    <!-- Top red Bar -->
+                    <div class="col-12" style="background-color:red;">
+                        <div class="row ">
+                            <div class="col-4">
+                            </div>
+                            <div class="col-4 text-center mt-2 mb-2 ">
+                                <label class=" Number visually-hidden  " id="Number">0</label>
+                                <span class="FlexTopRedBarTopic">Exclusive Suppliment🔥</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Flex Header -->
+                    <div class="col-12 position-sticky top-0 " style="z-index: 4;">
+                        <div class="row">
+                            <?php include "FlexHeader.php" ?>
+                        </div>
+                    </div>
+
+                    <!-- Content -->
+                    <div class="col-12 bg-dark">
+                        <div class="row">
+                            <div class="col-8 offset-lg-2">
+                                <div class="row">
+                                    <div class="col-12 pt-4 pb-4">
+                                        <h1 class="fw-bold fs-1 text-white">Contact</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-8 offset-lg-2 mt-4 mb-5">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-floating mb-3 bg-black text-white">
+                                            <input type="email" class="form-control bg-black text-white" id="floatingInput" placeholder="Name">
+                                            <label for="floatingInput" class="">Name</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-floating mb-3 bg-black text-white">
+                                            <input type="email" class="form-control bg-black text-white" id="floatingInput" placeholder="Email">
+                                            <label for="floatingInput" class="">Email</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3 bg-black text-white">
+                                            <input type="email" class="form-control bg-black text-white" id="floatingInput" placeholder="Phone Number">
+                                            <label for="floatingInput" class="">Phone Number</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <div class="form-floating bg-black text-white">
+                                            <textarea class="form-control bg-black text-white" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                                            <label for="floatingTextarea2">Comments</label>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-12 d-flex justify-content-center align-items-center">
+                                        <Button class="ContactSendBtn ">Send</Button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    <?php include "footer.php" ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-12">
+        <div class="row">
+            <div class="offcanvas  bg-black text-white offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title text-white" id="staticBackdropLabel" type="button" class="btn-close btn-danger" data-bs-dismiss="offcanvas" aria-label="Close"><i class="bi bi-x-lg"></i></h5>
+                    <!-- <button type="button" class="btn-close btn-danger"  data-bs-dismiss="offcanvas" aria-label="Close"></button> -->
+                </div>
+                <div class="offcanvas-body  position-relative">
+                    <div class="col-12 ">
+                        <div class="row">
+                            <!-- titile -->
+                            <div class="col-12 text-center">
+                                <h2>Your Cart</h2>
+                            </div>
+                            <!-- Sections -->
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-6 text-start">
+                                        <small class="text-white-50">Product</small>
+                                    </div>
+                                    <div class="col-6 text-end">
+                                        <small class="text-white-50">Total</small>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Product Details -->
+                            <div class="col-12">
+                                <div class="row">
+
+                                    <div class="col-4">
+                                        <img src="Resources/images/Suppliment1.jpg" class="cartProductImage" alt="cartSuppliment">
+                                    </div>
+
+                                    <div class="col-5">
+                                        <span class="fw-bold text-white">WHEY Premeum High Quauty Weigt Protin</span>
+                                        <br>
+                                        <span class="text-white-50">Rs.8,500</span>
+                                    </div>
+                                    <div class="col-3 text-end">
+                                        <span class="text-white fw-bold">Rs.8,500</span>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Quntity -->
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-4"></div>
+                                    <div class="col-7 ">
+                                        <div class="row">
+                                            <div class="col-8 mt-3 px-3 py-3 pt-3 pb-3 border border-1 border-white">
+                                                <div class="row">
+                                                    <div class="col-4">
+                                                        <span class="fw-bold text-white">-</span>
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <span class="fw-bold text-white">1</span>
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <span class="fw-bold text-white">+</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-4 d-flex  justify-content-center align-items-center">
+                                                <span><i class="bi bi-trash3"></i></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Checkout out -->
+                            <div class="col-12 CheckoutCover">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <hr class="text-white text-center">
+                                    </div>
+                                    <div class="col-12 mt-3">
+                                        <div class="row">
+                                            <div class="col-6 text-start">
+                                                <span class="fw-bold text-white">Estimated total</span>
+                                            </div>
+                                            <div class="col-6 text-end">
+                                                <span class=" text-white">Rs.8,500</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <small class="text-white-50">Taxes, Discounts and shipping calculated at checkout</small>
+                                    </div>
+                                    <div class="col-12 p-3 d-grid">
+                                        <button class="cartCheckoutBtn">Check out</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="js/script.js">
+    </script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
