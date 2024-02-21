19b0c06ee5f3a5eb59dc5e30e8e93fd6c97bef9d
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-02-21 21:05:44 +0530

    responsive Flex Logo

--- FlexHeader.php
+++ FlexHeader.php
@@ -18,13 +18,13 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="row">
-                            <div class="col-4 d-flex  align-items-center">
+                            <div class="col-lg-4 col-2 d-flex  align-items-center">
                                 <span class="fw-bold fs-4 offset-4 FlexHeadrTab hedaerIcon"><i class="bi bi-search"></i></span>
                             </div>
-                            <div class="col-4 text-center d-flex justify-content-center">
+                            <div class="col-lg-4 col-8 text-center d-flex justify-content-center">
                                 <img src="Resources/images/LOGO/Flex.jpg" class="FlexLOGO" alt="Flex Logo">
                             </div>
-                            <div class="col-4 text-end d-flex align-items-center ">
+                            <div class="col-lg-4 col-2 text-end d-flex align-items-center ">
                                 <span class="fs-1 fw-bold offset-5 FlexHeadrTab  "  type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop"><i class="bi bi-cart4"></i></span>
                             </div>
                         </div>
