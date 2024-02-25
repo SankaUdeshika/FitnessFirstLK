0ec655820438d2eca4b1decf66b999040d221fcc
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-02-25 19:03:11 +0530

    Fixed Error in Flex Connection

--- Connections/FlexConnection.php
+++ Connections/FlexConnection.php
@@ -1,32 +1,33 @@
 <?php
 
-class FlexDatabase{
+class FlexDatabase
+{
     public static $connection;
 
     public static function setUpConnections()
     {
-        if (!isset(Database::$connection)) {
-            Database::$connection = new mysqli("localhost", "root", "12345678", "flex_db", "3306");
+        if (!isset(FlexDatabase::$connection)) {
+            FlexDatabase::$connection = new mysqli("localhost", "root", "12345678", "flex_db", "3306");
         }
     }
 
     public static function iud($q)
     {
-        Database::setUpConnections();
-        Database::$connection->query($q);
+        FlexDatabase::setUpConnections();
+        FlexDatabase::$connection->query($q);
     }
 
     public static function search($q)
     {
-        Database::setUpConnections();
-        $resultSet = Database::$connection->query($q);
+        FlexDatabase::setUpConnections();
+        $resultSet = FlexDatabase::$connection->query($q);
         return $resultSet;
     }
 
-    public static function escape($Text){
-        Database::setUpConnections();
-       $TextResults = Database::$connection->real_escape_string($Text);
+    public static function escape($Text)
+    {
+        FlexDatabase::setUpConnections();
+        $TextResults = FlexDatabase::$connection->real_escape_string($Text);
         return $TextResults;
     }
-
 }
