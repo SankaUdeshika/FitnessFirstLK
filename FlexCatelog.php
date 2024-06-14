9a3282d28d2c6e83ba7ec8a18aa90cf9b44cb822
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-06-14 15:09:55 +0530

    Response the Flex Footer

--- FlexCatelog.php
+++ FlexCatelog.php
@@ -57,7 +57,7 @@ if (!isset($_COOKIE["User"])) {
                     <div class="col-12">
                         <div class="row">
                             <div class="col-lg-12 ">
-                                <img src="Resources/images/carouselImages/flexx03 1.png" class="FlexHomeImagecatelog" width="100%" >
+                                <img src="Resources/images/carouselImages/flexx03 1.png" class="FlexHomeImagecatelog" width="100%">
                             </div>
                         </div>
                     </div>
@@ -176,13 +176,7 @@ if (!isset($_COOKIE["User"])) {
                                                 </div>
                                             <?php
                                             }
-
                                             ?>
-
-
-
-
-
                                         </div>
                                     </div>
                                 </div>
@@ -194,7 +188,7 @@ if (!isset($_COOKIE["User"])) {
             </div>
             <div class="col-12">
                 <div class="row">
-                    <?php include "footer.php" ?>
+                    <?php include "flexfooter.php" ?>
                 </div>
             </div>
         </div>
--- flexfooter.php
+++ flexfooter.php
@@ -19,12 +19,99 @@
                         <hr class="text-white fw-bold">
                     </div>
 
-                    <div class="col-5  text-white-50">
+
+
+                    <!-- Large Screens -->
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-6 d-flex justify-content-center align-items-center">
+                                <div class="row">
+
+                                    <div class="col-lg-6 col-10 offset-0 offset-lg-1">
+                                        <span class="text-white fs-5">Highlight benefits of each exercise, both physical and mental</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <span class="fw-bold fs-5 text-white">Sitemap</span>
+                                            </div>
+                                            <div class="col-12 mt-2">
+                                                <span class="fw-bold fs-6 text-white-50">About Us</span>
+                                            </div>
+                                            <div class="col-12 ">
+                                                <span class="fw-bold fs-6 text-white-50">proms</span>
+                                            </div>
+                                            <div class="col-12 ">
+                                                <span class="fw-bold fs-6 text-white-50">New & Blog</span>
+                                            </div>
+                                            <div class="col-12 ">
+                                                <span class="fw-bold fs-6 text-white-50">Help Center</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <span class="fw-bold fs-5 text-white">Support</span>
+                                            </div>
+                                            <div class="col-12 mt-2">
+                                                <span class="fw-bold fs-6 text-white-50">FAQ</span>
+                                            </div>
+                                            <div class="col-12 ">
+                                                <span class="fw-bold fs-6 text-white-50">Support Center</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <span class="fw-bold fs-5 text-white">Social Media</span>
+                                            </div>
+                                            <div class="col-2 fs-2 d-lg-block d-none"> <span class="text-white"><i class="bi bi-instagram"></i></span></div>
+                                            <div class="col-2 fs-2 d-lg-block d-none"> <span class="text-white"><i class="bi bi-twitter"></i></span></div>
+                                            <div class="col-2 fs-2 d-lg-block d-none"> <i class="bi bi-facebook"></i></span></div>
+                                            <div class="row d-lg-none d-block">
+                                                <div class="col-12 text-center">
+                                                    <div class="col-2 fs-6 text-center"> <span class="text-white"><i class="bi bi-instagram"></i></span></div>
+                                                </div>
+                                                <div class="col-12 text-center">
+                                                    <div class="col-2 fs-6 text-center "> <span class="text-white"><i class="bi bi-twitter"></i></span></div>
+                                                </div>
+                                                <div class="col-12 text-center">
+                                                    <div class="col-2 fs-6 text-center "> <i class="bi bi-facebook"></i></span></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <hr class="border border-1 border-white">
+                            </div>
+
+                            <div class="col-12 text-center text-white-50">
+                                <span>
+                                    © 2023 FITNESS FIRST LK. All Right Reserved Developed By Hansa&Sanka</span>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <!-- Small Screens -->
+                    <div class="col-5  text-white-50 d-lg-none d-block">
                         <small class=" text-start mx-2 fw-bold">
                             ©FITNESS FIRST LK</small>
                     </div>
 
-                    <div class="col-7   text-white-50">
+                    <div class="col-7   text-white-50 d-lg-none d-block">
                         <div class="row">
                             <div class="col-12">
                                 <div class="row">
