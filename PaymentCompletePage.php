9625028731e6c87685df2ff6ff977dc76ac7aa1b
SankaUdeshika <sankaudeshika123@gmail.com>
2025-06-16 18:27:53 +0530

    Renew the Ui

---
+++ PaymentCompletePage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/flaticon.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body onload="">
+    <div class="col-12">
+        <div class="row">
+            <div class="col-12 " style=" width: 100%; height: 100vh;  display: flex; justify-content: center; align-items: center;">
+                <h1 style="justify-content: center; align-items: center;">Payment is Complete</h1>
+                <br>
+                <p>Please Wait</p>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
